3200f1fa3a67b7e554b09b3f9c8eefeaefb6c2fb
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-08 16:03:26 -0500

    modal up

--- src-client/scripts/new-chirp-component.js
+++ src-client/scripts/new-chirp-component.js
@@ -4,13 +4,36 @@ import {ACTIONS} from './actions.js'
 
 export const NewChirpComponent=React.createClass({
 
+  getInitialState: function(){
+    return {
+      showAlert: false
+    }
+  },
+  _removeModal: function(){
+    this.setState({showAlert: false})
+
+  },
+  _renderAlert: function(){
+    if(this.state.showAlert){
+      return(
+        <div className="modal-wrapper" onClick={this._removeModal}>
+          <div className="modal">
+            <h1>ERROR</h1>
+            <p>Chirp may not be longer than 140 characters</p>
+          </div>
+        </div>
+      )
+    }
+  },
+
   _handleNewChirp: function(evt){
 		evt.preventDefault();
     let formValsObj={}
     let formEl=evt.target;
     if (formEl.inputMessageEl.value.length > 140){
-      alert("Chirp may not be longer than 140 characters")
-      msg: ""
+      // alert("Chirp may not be longer than 140 characters")
+      // msg: ""
+      this.setState({showAlert: true})
     } else {
     formValsObj= {
       msg: formEl.inputMessageEl.value
@@ -28,8 +51,9 @@ render: function(){
 
   return(
     <div className="newChirp-container">
+      {this._renderAlert()}
       <form onSubmit={this._handleNewChirp}>
-        <input className="chirpInput" type="text" className="form-control" name="inputMessageEl"  />
+        <textarea className="chirpInput" type="text" className="form-control" name="inputMessageEl"  />
         <button className="chirpButton" type="submit">Chirp This</button>
       </form>
     </div>
